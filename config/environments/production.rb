78d410566eec9cb049f2aded12e446525eb78433
Theodore <teqq770@gmail.com>
2017-03-02 23:10:19 +0300

    123

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.assets.precompile += %w[tinymce/tinymce.min.js tinymce/langs/ru.js tinymce/themes/modern/theme.min.js]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
